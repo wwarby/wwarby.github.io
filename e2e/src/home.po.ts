2d88a97bec89f82009f8b8e2e4854302254fb2b5
William Warby <mail@wwarby.com>
2018-08-18 07:53:05 +0100

    Fix e2e test

--- e2e/src/home.po.ts
+++ e2e/src/home.po.ts
@@ -1,24 +1,28 @@
 import { browser, by, element } from 'protractor';
 
+/* tslint:disable:promise-function-async */
+
 export class HomePage {
 
-  public async navigateTo() {
+  public navigateTo() {
     return browser.get('/');
   }
 
-  public async getMainHeading() {
+  public getMainHeading() {
     return element(by.css('app-root h1')).getText();
   }
 
-  public async clickPhoto(photo: 'left' | 'middle' | 'right') {
-    return element(by.css(`app-root .photography img.${photo}`)).click();
+  public clickPhoto(photo: 'left' | 'middle' | 'right') {
+    element(by.css(`app-root .photography img.${photo}`)).click();
+    browser.sleep(50);
   }
 
-  public async closeGallery() {
-    return element(by.css('ngx-image-gallery .close')).click();
+  public closeGallery() {
+    element(by.css('ngx-image-gallery .close')).click();
+    browser.sleep(50);
   }
 
-  public async getActiveImageSrc() {
+  public getActiveImageSrc() {
     return element(by.css(`ngx-image-gallery .image.active img`)).getAttribute('src');
   }
 
